68c9861818ba621b193a1116dd7b284505a16e78
pareekankit <nishaypareek12@gmail.com>
2022-08-05 13:11:36 +0530

    unused function removed

--- ludo.js
+++ ludo.js
@@ -15,7 +15,7 @@ var Blue = [27, 28, 29,30, 31, 32, 33, 34,35, 36, 37, 38, 39, 40, 41, 42, 43, 44
     
 var Yellow = [14, 15, 16, 17, 18, 19,20, 21, 22, 23, 24, 25, 26, 27, 28, 29,30, 31, 32, 33, 34, 35, 36, 37, 38, 39,40, 41, 42, 43, 44, 45,
           46, 47, 48, 49,50, 51,0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10, 11, 12,57,58,59,60,61,72]; 
-          
+
 var path=[
     {
         name:"Red"
@@ -252,14 +252,7 @@ var path=[
                             }  
                         }
 
-    function playerTokentapped(token,imgid){
-        let tem;
-        tem=token.split("-");
-        tem.splice(1,1);
-        tem=tem.join("");
-        console.log(token);
-        updateposition(token,tem);
-    }
+
         
 function design(){
     // var img= document.createElement("img");
