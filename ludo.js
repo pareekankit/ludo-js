d5e77cb7fa7e4648f9728a8c84953933c6ac6453
pareekankit <nishaypareek12@gmail.com>
2022-08-10 19:58:47 +0530

    message

--- ludo.js
+++ ludo.js
@@ -1,13 +1,13 @@
 var mainpath=[];
 var dice;
 var tkoenId=[];
-var id=[50,51,0,49,52,1,48,53,2,47,54,3,46,55,4,48,56,5,39,40,41,42,43,44,6,7,8,9,10,11,38,67,68,69,70,71,72,61,60
+var id=[50,51,0,49,52,1,48,53,2,47,54,3,46,55,4,45,56,5,39,40,41,42,43,44,6,7,8,9,10,11,38,67,68,69,70,71,72,61,60
     ,59,58,57,12,37,36,35,34,33,32,18,17,16,15,14,13,31,66,19,30,65,20,29,64,21,28,63,22,27,62,23,26,25,24]; 
 var path=[
     {
         name:"Red",
         path:[40,41,42,43,44,45,46,47,48,49,50,51,0,1,2,3,4,5,6,7,8,9,10,11, 12, 13, 14, 15, 16, 17, 18, 19,
-            20, 21, 22, 23, 24, 25, 26, 27, 28, 29,30, 31, 32, 33, 34, 35, 36, 37, 38,67,68,69,70,71,72]
+            20, 21, 22, 23, 24, 25, 26, 27, 28, 29,30, 31, 32, 33, 34, 35, 36, 37, 38,67,68,69,70,71,73]
     },
     {
         name:"Green",
@@ -68,25 +68,25 @@ var path=[
                     id: 1,
                     name: 'Green-1',
                     position: -1,
-                    img:` <div><img src="doodles/Green.png" class="place" id=Green-1" onclick="updateposition('Green-1','Green')"/></div>`
+                    img:` <div><img src="doodles/Green.png" class="place" id=Green-1" onclick="updateposition('Green-1','green')"/></div>`
                      },
                 {
                     id: 2,
                     name: 'Green-2',
                     position: -1,
-                    img:` <div><img src="doodles/Green.png" class="place" id=Green-2" onclick="updateposition('Green-2','Green')"/></div>`
+                    img:` <div><img src="doodles/Green.png" class="place" id=Green-2" onclick="updateposition('Green-2','green')"/></div>`
                      },
                 {
                     id: 3,
                     name: 'Green-3',
                     position: -1,
-                    img:` <div><img src="doodles/Green.png" class="place" id=Green-3" onclick="updateposition('Green-3','Green')"/></div>`
+                    img:` <div><img src="doodles/Green.png" class="place" id=Green-3" onclick="updateposition('Green-3','green')"/></div>`
                      },
                 {
                     id: 4,
                     name: 'Green-4',
                     position: -1,
-                    img:` <div><img src="doodles/Green.png" class="place" id=Green-2" onclick="updateposition('Green-4','Green')"/></div>`
+                    img:` <div><img src="doodles/Green.png" class="place" id=Green-2" onclick="updateposition('Green-4','green')"/></div>`
                      }
             ]
         },
@@ -98,23 +98,27 @@ var path=[
                 {
                     id: 1,
                     name: 'Yellow-1',
-                   position: -1,
+                    position: -1,
+                    img:` <div><img src="doodles/Yellow.png" class="place" id=Yellow-1" onclick="updateposition('Yellow-1','yellow')"/></div>`
                    
                 },
                 {
                     id: 2,
                     name: 'Yellow-2',
                    position: -1,
+                   img:` <div><img src="doodles/Yellow.png" class="place" id=Yellow-2" onclick="updateposition('Yellow-2','yellow')"/></div>`
                 },
                 {
                     id: 3,
                     name: 'Yellow-3',
                    position: -1,
+                   img:` <div><img src="doodles/Yellow.png" class="place" id=Yellow-3" onclick="updateposition('Yellow-3','yellow')"/></div>`
                 },
                 {
                     id: 4,
                     name: 'Yellow-4',
                    position: -1,
+                   img:` <div><img src="doodles/Yellow.png" class="place" id=Yellow-4" onclick="updateposition('Yellow-4','yellow')"/></div>`
                 }
             ]
         },
@@ -127,21 +131,25 @@ var path=[
                     id: 1,
                     name: 'Blue-1',
                     position: -1,
+                    img:` <div><img src="doodles/Blue.png" class="place" id=Blue-1" onclick="updateposition('Blue-1','blue')"/></div>`
                 },
                 {
                     id: 2,
                     name: 'Blue-2',
                     position: -1,
+                    img:` <div><img src="doodles/Blue.png" class="place" id=Blue-2" onclick="updateposition('Blue-2','blue')"/></div>`
                 },
                 {
                     id: 3,
                     name: 'Blue-3',
                     position: -1,
+                    img:` <div><img src="doodles/Blue.png" class="place" id=Blue-3" onclick="updateposition('Blue-3','blue')"/></div>`
                 },
                 {
                     id: 4,
                     name: 'Blue-4',
                     position: -1,
+                    img:` <div><img src="doodles/Blue.png" class="place" id=Blue-4" onclick="updateposition('Blue-4','blue')"/></div>`
                 }
             ]
         },
@@ -173,18 +181,27 @@ var path=[
                 for(let j=0;j<playersData[i].tokenData.length;j++){
                         if(playersData[i].tokenData[j].name==pos){
                                 if( playersData[i].tokenData[j].position>=0){
-                                    let index=path[i].path.indexOf(playersData[i].tokenData[j].position);
-                                    console.log(path[i].path);
-                                    index=index+dice;
-                                    playersData[i].tokenData[j].position=path[i].path[index];
+                                    let myPath = path.find(e => e.name == playersData[i].color);
+                                    console.log(myPath, myPath.path.indexOf(playersData[i].tokenData[j].position))
+
+                                    if( myPath.path.indexOf(playersData[i].tokenData[j].position) +dice < 57){
+                                        let index=path[i].path.indexOf(playersData[i].tokenData[j].position);
+                                        console.log(path[i].path);
+                                        index=index+dice;
+                                        playersData[i].tokenData[j].position=path[i].path[index];
                                     index=path[i].path[index]
                                     console.log("position",playersData[i].tokenData[j].position)
                                     upadtMainPathvalueAtindex(index,pos,playersData[i].color);
                                     turn();
+                                    }
+                                    else{
+                                        turn(); 
+                                    }
+                                    
                             }
                            
                         else{
-                            if(dice==6){
+                            if(dice==6){ 
                                 playersData[i].tokenData[j].position=path[i].path[0];
                                 if( mainpath[path[i].path[0]]){
                                     console.log("if");
@@ -212,7 +229,6 @@ var path=[
 
                 
         function upadtMainPathvalueAtindex(index,value,color){
-            console.log(color);
             var valueAtIndex;
                     valueAtIndex=mainpath[index];
                 if (valueAtIndex) {
@@ -220,14 +236,12 @@ var path=[
                             console.log("add");
                             remove_elements(value)
                             mainpath[index] +=`,${value}`;
-
+                            document.getElementById(index).innerHTML="";
                             (mainpath[index].split(',')).forEach(element => {
-                                debugger;
                                 let img = document.createElement('img');
                                 img.src = '/doodles/' + element.split('-')[0] + '.png';
                                 img.id = element;
                                 img.classList.add('place');
-        
                                 img.addEventListener('click', () => {
                                         let currentPlayer = playersData.find(e => { 
                                                                                     return (element).split('-')[0] == e.color
@@ -241,36 +255,38 @@ var path=[
                                 div.appendChild(img);
                                 document.getElementById(index).appendChild(div);
         
+                            });
+                        }
+                        else{
+                            debugger;
+                            remove_elements(value);
+                            mainpath[index] +=`,${value}`;
+                            document.getElementById(index).innerHTML="";
+                        if(index==35||index==40||index==48||index==1||index==9||index==14||index==22||index==27||index==72){
+                            (mainpath[index].split(',')).forEach(element => {
+                                let img = document.createElement('img');
+                                img.src = '/doodles/' + element.split('-')[0] + '.png';
+                                img.id = element;
+                                img.classList.add('place');
+                                img.addEventListener('click', () => {
+                                        let currentPlayer = playersData.find(e => { 
+                                                                                    return (element).split('-')[0] == e.color
+                                                                                    });
+        
+                                let currentToken = currentPlayer.tokenData.find(e => e.name == element);
+                                console.log( currentToken.name, currentPlayer.color)
+                                    updateposition( currentToken.name, currentPlayer.color)
+                                })
+                                let div = document.createElement('div');
+                                div.appendChild(img);
+                                document.getElementById(index).appendChild(div);
                             });
                         }
                         else{
                             remove_elements(value);
-                        if(index==35||index==40||index==48||index==1||index==9||index==14||index==22||index==27){
+                            mainpath[index]=value;
+                            document.getElementById(index).innerHTML="";         
                             (mainpath[index].split(',')).forEach(element => {
-                                debugger;
-                                let img = document.createElement('img');
-                                img.src = '/doodles/' + element.split('-')[0] + '.png';
-                                img.id = element;
-                                img.classList.add('place');
-        
-                                img.addEventListener('click', () => {
-                                        let currentPlayer = playersData.find(e => { 
-                                                                                    return (element).split('-')[0] == e.color
-                                                                                    });
-        
-                                let currentToken = currentPlayer.tokenData.find(e => e.name == element);
-                                console.log( currentToken.name, currentPlayer.color)
-                                    updateposition( currentToken.name, currentPlayer.color)
-                                })
-                                let div = document.createElement('div');
-                                div.appendChild(img);
-                                document.getElementById(index).appendChild(div);
-        
-                            });
-                        }
-                        else{
-                            (mainpath[index].split(',')).forEach(element => {
-                                debugger;
                                 let img = document.createElement('img');
                                 img.src = '/doodles/' + element.split('-')[0] + '.png';
                                 img.id = element;
@@ -296,18 +312,17 @@ var path=[
                     else {      
                                 remove_elements(value);
                                 mainpath[index]=value;
+                                debugger;
                                 (mainpath[index].split(',')).forEach(element => {
-                                    debugger;
+                                    
                                     let img = document.createElement('img');
                                     img.src = '/doodles/' + element.split('-')[0] + '.png';
                                     img.id = element;
-                                    img.classList.add('place');
-            
+                                    img.classList.add('place');            
                                     img.addEventListener('click', () => {
                                             let currentPlayer = playersData.find(e => { 
                                                                                         return (element).split('-')[0] == e.color
                                                                                         });
-            
                                     let currentToken = currentPlayer.tokenData.find(e => e.name == element);
                                     console.log( currentToken.name, currentPlayer.color)
                                         updateposition( currentToken.name, currentPlayer.color)
@@ -335,14 +350,8 @@ var path=[
                     ov.splice(i,1);
                     ov = ov.join();
                     }
-        
-                    console.log("ov",ov);
-
                     mainpath[indexx]= ov;
                     ov=ov.split(",");
-                    // ov=ov.replace(" ","");
-                    console.log("ov",ov);
-
                     document.getElementById(indexx).innerHTML = '';
                     if(ov==''){
                         document.getElementById(indexx).innerHTML = '';  
@@ -374,7 +383,7 @@ var path=[
                        
                 }
                     return indexx;
-                }
+            }
                
             function design(){
                 var div=[];
@@ -435,6 +444,17 @@ var path=[
                 for( let k=0;k<tkoenId.length;k++){
                     document.getElementById(tkoenId[k]).addEventListener('click',()=>{playerTokentapped(tkoenId[k])});
                 }
+                var win=[]
+                for(let i=0;i<4;i++){
+                    win.push(document.createElement('div'));
+                    win[i].id=i+73;
+                    document.getElementById(72).appendChild(win[i]);
+                }
+                win[0].classList.add('redwin');
+                win[1].classList.add('greenwin');
+                win[2].classList.add('bluewin');
+                win[3].classList.add('yellowwin');
+                console.log(win);
             }
             function playerTokentapped(token,imgid){
                 let tem;
